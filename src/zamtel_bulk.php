d41feaa0fae08f299fa4c190bb4d35d692f54238
kazashim <kazashimkuzasuwat@gmail.com>
2020-07-04 22:40:42 +0200

    u

--- src/zamtel_bulk.php
+++ src/zamtel_bulk.php
@@ -27,16 +27,17 @@ private function send_server_response($url,$post_body){
      * multipart formpost, which is not supported:
      */
 
-    $ch = curl_init( );
-    curl_setopt ( $ch, CURLOPT_URL, $url );
-    curl_setopt ( $ch, CURLOPT_POST, 1 );
-    curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, 1 );
-    curl_setopt ( $ch, CURLOPT_POSTFIELDS, $post_body );
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
     // Allowing cUrl functions 20 second to execute
-    curl_setopt ( $ch, CURLOPT_TIMEOUT, 20 );
+    curl_setopt($ch, CURLOPT_TIMEOUT, 20);
     // Waiting 20 seconds while trying to connect
-    curl_setopt ( $ch, CURLOPT_CONNECTTIMEOUT, 20 );
-    $response_string = curl_exec( $ch );
-    curl_close( $ch );
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 20);
+    $response_string = curl_exec($ch);
+    curl_close($ch);
 
     return $response_string;
+}
