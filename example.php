2e5d232e7237b8b7f68e285f1f98a557799be6f5
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2020-07-27 19:01:26 +0200

    created sample code

--- example.php
+++ example.php
@@ -16,3 +16,56 @@
 
 // Step 1: Get the Cyn SMS API library from https://github.com/kazashim/zamtel-bulksms-php-sdk,
 // following the instructions to install it with Composer.
+
+require_once 'src/Zamtel_bulk.php';
+use ZamtelBulk\ZamtelBulkAPI;
+
+
+// Step 2: set your API_KEY from https://mywebhost.com/sms-api/info
+
+$key = 'eeb45fcda355161e419c5763f8b943ec';
+
+
+// Step 3: Change the from number below. It can be a valid phone number or a String
+$senderid = 'DCTalkRadio';
+
+// Step 4: the number we are sending to - Any phone number
+$contacts = '260965058568';
+
+// Step 5: Replace your Install URL like https://mywebhost.com/sms/api with https://http://bulksms.zamatel.co.zm/api/sms/send/batch?
+// <sms/api> is mandatory.
+
+$url = 'https://bulksms.zamtel.co.zm/api/sms/send/batch';
+
+// the sms body
+$message = 'test message from cynojine SMS';
+
+// unicode sms
+$unicode = 0; //For Plain Message
+$unicode = 1; //For Unicode Message
+
+
+// Create SMS Body for request
+$sms_body = array(
+    'key' => $key,
+    'contacts' => $contacts,
+    'senderid' => $senderid,
+    'message' => $message,
+    
+);
+
+// Step 6: instantiate a new Ultimate SMS API request
+$client = new ZamtelBulkAPI();
+
+// Step 7: Send SMS
+$response = $client->send_sms($sms_body, $url);
+
+print_r($response);
+
+
+// Step 8: Get Response
+$response=json_decode($response);
+
+// Display a confirmation message on the screen
+//echo 'Message: '.$response->message;
+
